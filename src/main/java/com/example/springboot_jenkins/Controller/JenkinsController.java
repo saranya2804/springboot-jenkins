a169a7675ca7e42c2994f8cfbc4fd8c110ac28bd
91799 <91799@DESKTOP-BP4C19V>
2024-07-02 18:47:38 +0530

    firstcommit

---
+++ src/main/java/com/example/springboot_jenkins/Controller/JenkinsController.java
@@ -0,0 +1,11 @@
+package com.example.springboot_jenkins.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class JenkinsController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Jenkins";
+    }
+
+}
